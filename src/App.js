76ff96bad39f22dc8feaecddaa2eb3c6ec8c996c
최정훈 <jeongssi94@gmail.com>
2022-10-29 22:50:19 +0900

    Add:기능 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 function App() {
+  const [todos, seTodos] = useState([
+    {
+      id: 1,
+      text: '리엑트의 기초 학습',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '리엑트의 컴포넌트 학습2',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들기 학습3',
+      checked: false,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }

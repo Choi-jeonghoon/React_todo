9c0b3508647a672b42bea46ac85f48a8122c8133
최정훈 <jeongssi94@gmail.com>
2022-10-31 18:46:50 +0900

    Add:많은 데이터 렌더링및 크롬 도구의 성능 모니터링

--- src/App.js
+++ src/App.js
@@ -3,28 +3,42 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
       id: 1,
-      text: '리엑트의 기초 학습',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '리엑트의 컴포넌트 학습2',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들기 학습3',
+      text: `할일${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
+
+function App() {
+  const [todos, setTodos] = useState(createBulkTodos);
+  // function App() {
+  //   const [todos, setTodos] = useState([
+  //     {
+  //       id: 1,
+  //       text: '리엑트의 기초 학습',
+  //       checked: true,
+  //     },
+  //     {
+  //       id: 2,
+  //       text: '리엑트의 컴포넌트 학습2',
+  //       checked: true,
+  //     },
+  //     {
+  //       id: 3,
+  //       text: '일정 관리 앱 만들기 학습3',
+  //       checked: false,
+  //     },
+  //   ]);
   //고윳값으로 사양될 id
   //ref를 사용하여 변수 담기
-  const nextId = useRef(4);
-
+  // const nextId = useRef(4);
+  const nextId = useRef(2501);
   const onInsert = useCallback(
     (text) => {
       const todo = {
